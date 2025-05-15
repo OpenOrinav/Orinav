ca51c2b213c441f7725ff86284b386263f8a4c31
Dreta <32196824+Dreta@users.noreply.github.com>
2025-05-15 09:57:01 +0800

    Initial commit

---
+++ BeaconNext/BeaconNextApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeaconNextApp.swift
+//  BeaconNext
+//
+//  Created by Dreta ​ on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct BeaconNextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

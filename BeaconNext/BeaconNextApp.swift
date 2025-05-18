47897748fcf48fad56ad99b155c36693567b568e
Dreta <32196824+Dreta@users.noreply.github.com>
2025-05-18 21:54:43 +0800

    feat: add basic navigation popup

--- BeaconNext/BeaconNextApp.swift
+++ BeaconNext/BeaconNextApp.swift
@@ -4,12 +4,14 @@ import SwiftUI
 struct BeaconNextApp: App {
     @StateObject private var locationManager = BeaconLocationDelegateSimple()
     @StateObject private var searchManager = BeaconSearchDelegateSimple()
+    @StateObject private var navigationManager = BeaconNavigationDelegateSimple()
     
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(locationManager)
                 .environmentObject(searchManager)
+                .environmentObject(navigationManager)
         }
     }
 }

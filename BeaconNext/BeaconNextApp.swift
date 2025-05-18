1e197284e872a5708cb3509d590ed77d6ce45516
Dreta <32196824+Dreta@users.noreply.github.com>
2025-05-18 10:02:15 +0800

    chore: revert route planning features

--- BeaconNext/BeaconNextApp.swift
+++ BeaconNext/BeaconNextApp.swift
@@ -4,14 +4,12 @@ import SwiftUI
 struct BeaconNextApp: App {
     @StateObject private var locationManager = BeaconLocationDelegateSimple()
     @StateObject private var searchManager = BeaconSearchDelegateSimple()
-    @StateObject private var routePlanManager = BeaconRoutePlanDelegateSimple()
     
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(locationManager)
                 .environmentObject(searchManager)
-                .environmentObject(routePlanManager)
         }
     }
 }

297af5a621149ab96a86704d2881a9810bd418b8
Dreta <32196824+Dreta@users.noreply.github.com>
2025-06-01 19:03:28 +0800

    feat: obstacle detection (not quite working!)

--- BeaconNext/Pages/Explore/BeaconExploreView.swift
+++ BeaconNext/Pages/Explore/BeaconExploreView.swift
@@ -1,15 +1,23 @@
 import SwiftUI
 
 struct BeaconExploreView: View {
-    var body: some View {
-        VStack {
-            
-        }
-        .navigationTitle("Explore")
-        .navigationBarTitleDisplayMode(.large)
-    }
-}
+    @StateObject private var obstacleDetector = BeaconObstacleDetector()
+    @State private var obstacleImage: CGImage?
 
-#Preview {
-    BeaconExploreView()
+    var body: some View {
+        ZStack {
+            // Camera view resized to 512x512 and allowed to stretch
+            CameraView { cgImage in
+                obstacleDetector.detect(cgImage)
+            }
+            .frame(width: 512, height: 512)
+            // Overlay the obstacle image semi-transparent on top
+            if let obstacleCG = obstacleDetector.obstacleImage {
+                Image(decorative: obstacleCG, scale: 1.0, orientation: .up)
+                    .resizable()
+                    .frame(width: 512, height: 512)
+                    .opacity(0.5)
+            }
+        }
+    }
 }

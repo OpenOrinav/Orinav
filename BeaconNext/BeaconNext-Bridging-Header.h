8b9f5003ae85410e88d7376f2b826dacfcfd450d
Dreta <32196824+Dreta@users.noreply.github.com>
2025-06-10 21:55:05 +0800

    partial: `BeaconObstacleDetector`

---
+++ BeaconNext/BeaconNext-Bridging-Header.h
@@ -0,0 +1,1 @@
+#include "ConnectedComponents.h"
--- BeaconNext/Pages/Explore/BeaconExploreView.swift
+++ BeaconNext/Pages/Explore/BeaconExploreView.swift
@@ -12,12 +12,19 @@ struct BeaconExploreView: View {
                 obstacleDetector.detect(cgImage, frame: frame)
             }
             .edgesIgnoringSafeArea(.all)
+            if let previewCG = obstacleDetector.originalImage {
+                Image(decorative: previewCG, scale: 1.0, orientation: .up)
+                    .resizable()
+                    .frame(width: 512, height: 512)
+                    .scaledToFill()
+            }
+            
             // Overlay the obstacle image semi-transparent on top
             if let obstacleCG = obstacleDetector.obstacleImage {
                 Image(decorative: obstacleCG, scale: 1.0, orientation: .up)
                     .resizable()
                     .frame(width: 512, height: 512)
-                    .opacity(0.5)
+                    .opacity(0.8)
             }
         }
     }

29fb204cfe2b6fa8ddf580d25fc9fdc32378f6f3
BenjaminJiang-create <Benjaminjiang20090130@outlook.com>
2025-06-19 09:29:53 +0800

    added localize function

--- BeaconNext/Pages/Settings/BeaconSettingsView.swift
+++ BeaconNext/Pages/Settings/BeaconSettingsView.swift
@@ -1,15 +1,34 @@
 import SwiftUI
 
 struct BeaconSettingsView: View {
+
+    var languageCode: String {
+        Locale.current.language.languageCode?.identifier ?? "Unknown"
+    }
+    
+    var localizedLanguageName: String {
+        Locale.current.localizedString(forIdentifier: languageCode) ?? languageCode
+    }
+    
     var body: some View {
-        VStack {
+        VStack(alignment: .leading, spacing: 16) {
+            Text("Current Language:")
+                .font(.headline)
             
+            Text(localizedLanguageName)
+                .font(.body)
+                .foregroundColor(.gray)
+            
+            Spacer()
         }
+        .padding()
         .navigationTitle("Settings")
         .navigationBarTitleDisplayMode(.large)
     }
 }
 
 #Preview {
-    BeaconSettingsView()
+    NavigationView {
+        BeaconSettingsView()
+    }
 }

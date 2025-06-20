fe789e6b6ac0e7ba9519cfecf1d474afa42cc6c3
Dreta <32196824+Dreta@users.noreply.github.com>
2025-06-20 13:55:16 +0800

    fix: debugging mapbox

--- BeaconNext/Library/Navigation Services/Mapbox/MapboxNavigationServiceProvider.swift
+++ BeaconNext/Library/Navigation Services/Mapbox/MapboxNavigationServiceProvider.swift
@@ -8,6 +8,8 @@ class MapboxNavigationServiceProvider: BeaconNavigationProvider {
     var delegate: (any BeaconNavigationProviderDelegate)?
     let mnp = MapboxNavigationProvider(coreConfig: .init())
     
+    var routes: NavigationRoutes? // DEBUG
+    
     var controller: NavigationViewController?
     
     var navView: UIView {
@@ -23,12 +25,13 @@ class MapboxNavigationServiceProvider: BeaconNavigationProvider {
         location: BeaconLocation
     ) async -> [any BeaconWalkRoute] {
         do {
-            return try await mnp.mapboxNavigation.routingProvider().calculateRoutes(
+            routes = try await mnp.mapboxNavigation.routingProvider().calculateRoutes(
                 options: NavigationRouteOptions(
                     coordinates: [from?.bCoordinate ?? location.bCoordinate, to?.bCoordinate ?? location.bCoordinate],
                     profileIdentifier: .walking
                 )
-            ).value.allRoutes().map { route in
+            ).value
+            return routes!.allRoutes().map { route in
                 MapboxRouteWrapper(
                     mapboxRoute: route,
                     origin: from ?? BeaconLocationPOIWrapper(location),
@@ -44,16 +47,18 @@ class MapboxNavigationServiceProvider: BeaconNavigationProvider {
     func clearState() {
     }
     
-    func startNavigation(with: any BeaconWalkRoute) {
-        navigationViewController = NavigationViewController(
-            navigationRoutes: [(with as! MapboxRouteWrapper).mapboxRoute],
-            navigationOptions: NavigationOptions(
-                mapboxNavigation: mnp.mapboxNavigation,
-                voiceController: mnp.routeVoiceController,
-                eventsManager: mnp.eventsManager()
+    func startNavigation(with: any BeaconWalkRoute) { // DEBUG
+        DispatchQueue.main.async {
+            self.controller = NavigationViewController(
+                navigationRoutes: self.routes!,
+                navigationOptions: NavigationOptions(
+                    mapboxNavigation: self.mnp.mapboxNavigation,
+                    voiceController: self.mnp.routeVoiceController,
+                    eventsManager: self.mnp.eventsManager()
+                )
             )
-        )
-        navigationViewController.modalPresentationStyle = .fullScreen
+            self.controller!.modalPresentationStyle = .fullScreen
+        }
     }
 }
 

66c8f4e1bd0f31451eb004ff07a8257964675e2b
Dreta <32196824+Dreta@users.noreply.github.com>
2025-06-20 11:02:51 +0800

    feat: `MapboxLocationProvider`

---
+++ BeaconNext/Library/Navigation Services/Mapbox/MapboxLocationProvider.swift
@@ -0,0 +1,81 @@
+import Foundation
+import CoreLocation
+import MapboxSearch
+
+class MapboxLocationProvider: NSObject {
+    var delegate: BeaconLocationProviderDelegate?
+    private let locationManager = CLLocationManager()
+
+    private let accessToken: String
+    
+    override init() {
+        guard let accessToken = Bundle.main.object(forInfoDictionaryKey: "MBXAccessToken") as? String else {
+            fatalError("Missing MBXAccessToken in Info.plist")
+        }
+        self.accessToken = accessToken
+        
+        super.init()
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.headingFilter = 5
+        locationManager.requestWhenInUseAuthorization()
+        startUpdating()
+    }
+
+    func startUpdating() {
+        guard CLLocationManager.locationServicesEnabled() else { return }
+        locationManager.startUpdatingLocation()
+        if CLLocationManager.headingAvailable() {
+            locationManager.startUpdatingHeading()
+        }
+    }
+    func stopUpdating() {
+        locationManager.stopUpdatingLocation()
+        locationManager.stopUpdatingHeading()
+    }
+}
+
+// MARK: - CLLocationManagerDelegate
+extension MapboxLocationProvider: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+            startUpdating()
+        default:
+            stopUpdating()
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let latest = locations.last else { return }
+        let placeAutocomplete = PlaceAutocomplete(accessToken: accessToken)
+        placeAutocomplete.suggestions(for: latest.coordinate) { result in
+            switch result {
+            case .success(let suggestions):
+                self.delegate?.didUpdateLocation(suggestions[0])
+
+            case .failure(let error):
+                print("MapboxLocationProvider error (Mapbox): \(error)")
+            }
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        delegate?.didUpdateHeading(newHeading.trueHeading >= 0 ? newHeading.trueHeading : newHeading.magneticHeading)
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("MapboxLocationProvider error: \(error.localizedDescription)")
+    }
+}
+
+extension PlaceAutocomplete.Suggestion: BeaconLocation {
+    var bCoordinate: CLLocationCoordinate2D {
+        return coordinate!
+    }
+    
+    var bName: String? {
+        return name
+    }
+}

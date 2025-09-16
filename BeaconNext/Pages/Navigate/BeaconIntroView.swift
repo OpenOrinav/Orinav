f60d49f7538d7aed6f68dc45c6f07f35489d51c3
Dreta <32196824+Dreta@users.noreply.github.com>
2025-09-16 21:37:41 +0800

    feat: better privacy protection

--- BeaconNext/Pages/Navigate/BeaconIntroView.swift
+++ BeaconNext/Pages/Navigate/BeaconIntroView.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct BeaconIntroView: View {
     @Binding var isPresented: Bool
     
-    // TODO: Rework
+    @EnvironmentObject var globalState: BeaconMappingCoordinator
     
     var body: some View {
         ScrollView {
@@ -18,18 +18,30 @@ struct BeaconIntroView: View {
                 Text("Orinav is a new navigation app designed for individuals with visual impairments. It provides intuitive navigation features to help you get around safely.")
                 Text("To get started navigating, simply search for places or addresses. While navigating, you will hear instructions to orient yourself to the road. Shake to repeat instructions.")
                 Text("Enter the Explore tab to learn more about your environment, including recognizing traffic lights, objects, and obstacles. While navigating, raise your phone to enter Explore mode.")
-                Text("Keep your phone pointing straight forward. When using Explore, keep your phone upright, at chest level, and pointing straight ahead.")
+                Text("Keep your phone pointing straight forward. When using Explore, keep your phone upright, at chest level, and pointing straight ahead. Hold the bottom half of your phone so that you won't obstruct the camera.")
                 Text("Orinav is not a medical device. Orinav should not be used as a sole means of navigation and does not replace service animals, mobility aids, orientation and mobility training, or professional medical and safety advice. You remain responsible for your own safety.")
                 Text("You can return to this introduction at any time from the home page.")
-                Text("By continuing, you indicate your agreement to Orinav's [Terms of Service](\("https://orinav.com/terms")) and [Privacy Policy](\("https://orinav.com/privacy")).") // Links separated to allow for localization
+                Text("By continuing, you indicate your agreement to Orinav's Terms of Service and Privacy Policy. Your location may be shared internationally with third-party map providers to provide navigation services. See full terms for details.")
+                Button("Open terms...") {
+                    if let url = URL(string: "https://orinav.com/terms") {
+                        UIApplication.shared.open(url)
+                    }
+                }
                 
                 Button("Agree and Continue") {
                     isPresented = false
+                    SettingsManager.shared.shownIntro = true
+                    globalState.locationProvider.requestPermissions() // The user has agreed to the privacy policy
                 }
                 .buttonStyle(.borderedProminent)
                 .controlSize(.large)
                 .frame(maxWidth: .infinity)
                 .clipShape(Capsule())
+                
+                Button("Disagree and Exit") {
+                    exit(0)
+                }
+                .frame(maxWidth: .infinity)
             }
             .frame(maxWidth: .infinity)
             .padding()
